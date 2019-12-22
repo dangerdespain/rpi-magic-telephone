a045bdc021bda5746f924146c29d9f711ee05a3c
Devin Despain <devin@dspa.in>
2019-12-22 16:53:00 -0700

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ KEYPAD = [
 ]
 
 ROW_PINS = [4, 14, 15, 17] # BCM numbering
-COL_PINS = [18, 27, 22] # BCM numbering
+COL_PINS = [23, 24, 25] # BCM numbering
 
 factory = rpi_gpio.KeypadFactory()
 
