c25d851e71ce4daa57c089fca6b4227ef43c4671
Devin Despain <devin@dspa.in>
2019-12-22 19:06:16 -0700

    fix

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ def printKey(key):
         FILE = 'words/PLEASURE'
 
     FILENAME = os.path.dirname(os.path.abspath(__file__))  + '/audio/speak_and_spell/' + FILE + '.wav'
-    os.system('omxplayer ' + FILENAME )
+    os.system('omxplayer ' + FILENAME + ' -o both')
     print(FILENAME)
         # wf = wave.open(FILENAME, 'rb')
 
