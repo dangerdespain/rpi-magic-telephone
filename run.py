5941aae70fea528943e3f6b9607c61a3f65c43bf
Devin Despain <devin@dspa.in>
2019-12-22 17:11:05 -0700

    casting key as string

--- run.py
+++ run.py
@@ -24,8 +24,8 @@ keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PI
 
 def printKey(key):
     print(key)
-    print('aplay ' + os.path.dirname(__file__) + '/speak_and_spell/alphabet/' + key +'.wav')
-    # os.system('aplay ' + os.path.dirname() + /home/pi/rpi-magic.wav')
+    print('aplay ' + os.path.dirname(__file__) + '/speak_and_spell/alphabet/.wav')
+    os.system('aplay ' + os.path.dirname(__file__) + '/speak_and_spell/alphabet/' + str(key) + '.wav')
 
 # printKey will be called each time a keypad button is pressed
 keypad.registerKeyPressHandler(printKey)
